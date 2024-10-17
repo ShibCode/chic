120c7137601610b97387612cc9274e77e1a982b9
ShibCode <shoaibafzaal1234@gmail.com>
2024-10-17 18:30:20 +0500

    .

--- pages/Home/InstagramFeed.jsx
+++ pages/Home/InstagramFeed.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const feedData = ["/feed-1.jpg", "/feed-2.jpg", "/feed-3.jpg", "/feed-4.jpg"];
 
 const getFeed = async () => {
+  if (!process.env.INSTAGRAM_KEY) return;
+
   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`;
 
   const data = await fetch(url);
+
   const feed = await data.json();
 
   return feed;
@@ -25,7 +28,7 @@ const InstagramFeed = async () => {
       </h3>
 
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 w-full">
-        {feed.data
+        {feed?.data
           .filter((post) => post.media_type === "IMAGE")
           .slice(0, 4)
           .map((post, i) => (
